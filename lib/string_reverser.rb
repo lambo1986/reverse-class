aa72f734f1201da8ae2bfac5509d348dbb43e290
Nathan Lambertson <lambertones@gmail.com>
2024-03-12 08:10:56 -0500

    Add custom reverse method with test

--- lib/string_reverser.rb
+++ lib/string_reverser.rb
@@ -1,5 +1,13 @@
 class StringReverser
   def initialize
-    
+    def reverse(str)
+      reversed = []
+      split = str.split('')
+      until split.empty?
+        letter = split.pop
+        reversed << letter
+      end
+      reversed.join('')
+    end
   end
 end
--- spec/string_reverser_spec.rb
+++ spec/string_reverser_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe StringReverser do
       expect(string_reverser).to be_a(StringReverser)
     end
   end
+
+  describe "#reverse" do
+    it "reverses a string" do
+      string_reverser = StringReverser.new
+
+      expect(string_reverser.reverse("hello")).to eq("olleh")
+    end
+  end
 end
